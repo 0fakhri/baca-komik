4413ef6703c7441bf13744d25fa1bfd9b6465a33
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-10-16 09:49:42 +0700

    kasih slider pro wkwkwk

---
+++ js/slider.js
@@ -0,0 +1,56 @@
+const bungkus = document.querySelector(".tengah");
+const  allbox = bungkus.children;
+console.log(allbox);
+let awal=0;
+let akhir=4;
+
+function kanan_klik(){
+  console.log("jalan kanan");
+  document.querySelector(".kiri_panah").style.display = "flex";
+  if(akhir<allbox.length-1){
+    awal+=1;
+    akhir+=1;
+  }
+  console.log(akhir);
+  if(awal == 0){
+    document.querySelector(".kiri_panah").style.display = "none";
+  } else{
+    document.querySelector(".kiri_panah").style.display = "flex";
+  }
+  if(akhir == allbox.length-1){
+    document.querySelector(".kanan_panah").style.display = "none";
+  } else {
+    document.querySelector(".kanan_panah").style.display = "flex";
+  }
+  for (let i = 0; i<= allbox.length-1;i++){
+    if(i>=awal && i<=akhir){
+      allbox[i].style.display = "inline-block";
+    } else{
+      allbox[i].style.display = "none";
+    }
+  }
+}
+function kiri_klik(){
+  console.log("jalan kiri");
+  if(awal>0){
+    awal-=1;
+    akhir-=1;
+  }
+  if(awal == 0){
+    document.querySelector(".kiri_panah").style.display = "none";
+  } else{
+    document.querySelector(".kiri_panah").style.display = "flex";
+  }
+  if(akhir == allbox.length-1){
+    document.querySelector(".kanan_panah").style.display = "none";
+  } else {
+    document.querySelector(".kanan_panah").style.display = "flex";
+  }
+  for (let i = 0; i<= allbox.length-1;i++){
+    if(i>=awal && i<=akhir){
+      allbox[i].style.display = "inline-block";
+    } else{
+      allbox[i].style.display = "none";
+    }
+  }
+}
