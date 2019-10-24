748ffbf449ca9f6d80ed2edea14d51682d080771
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-10-24 20:19:39 +0700

    dark-mode est lumayanlah

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -4,10 +4,13 @@ function toggleDarkLight(){
   console.log(isdark);
   if (isdark){
       document.documentElement.setAttribute('data-theme', 'light');
+      document.getElementById("dark-mode").style.background = "#ccc";
+      localStorage.setItem('theme', 'light');
       isdark = false;
     }
     else {
       document.documentElement.setAttribute('data-theme', 'dark');
+      document.getElementById("dark-mode").style.background = "#444";
       localStorage.setItem('theme', 'dark');
       isdark = true;
     }
