b38e62263d4f44f72c5060a4d24a1cf49dd35760
0fakhri <indsmnz15@gmail.com>
2019-10-24 14:42:17 +0700

    edit dan tambah

---
+++ js/lebihBanyak.js
@@ -0,0 +1,9 @@
+var z = document.getElementById("banyak");
+function show() {
+    if (z.style.display === "none") {
+      z.style.display = "block";
+    } 
+    else {
+      z.style.display = "none";
+    }
+  }
