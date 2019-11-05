8f4fb4467a2e90adc8a45faf90fc1a51e47259ff
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-11-05 17:18:46 +0700

    menambah jquery upload file

--- js/modalBox.js
+++ js/modalBox.js
@@ -1,4 +1,18 @@
 var z = document.getElementById("dropdown");
+
+function readURL(input) {
+       if (input.files && input.files[0]) {
+           var reader = new FileReader();
+
+           reader.onload = function (e) {
+               $('#blah')
+                   .attr('src', e.target.result);
+           };
+
+           reader.readAsDataURL(input.files[0]);
+       }
+   }
+
 function drop() {
      if (z.style.display === "none") {
        z.style.display = "block";
