771bd7296104b801b00fefad4e3be5d0db928621
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-11-12 13:55:25 +0700

    ubah js native ke jquery

--- js/nav_view.js
+++ js/nav_view.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos < currentScrollPos) {
-      document.getElementById("navbar").style.opacity = "0";
+    $("#navbar").css("opacity","0");
   } else {
-    document.getElementById("navbar").style.opacity = "1";
+    $("#navbar").css("opacity","1");
   }
   prevScrollpos = currentScrollPos;
 }
@@ -12,8 +12,8 @@ window.onscroll = function() {
 document.addEventListener('mousemove', logKey);
 function logKey(e) {
    if(e.clientY<88){
-     document.getElementById("navbar").style.opacity = "1";
+     $("#navbar").css("opacity","1");
    } else {
-     document.getElementById("navbar").style.opacity = "0";
+     $("#navbar").css("opacity","0");
    }
 }
