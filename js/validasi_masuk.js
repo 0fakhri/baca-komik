4eec6aa34a17d1dfda25d91520f393cb5abb3a73
0fakhri <indsmnz15@gmail.com>
2019-11-12 15:05:18 +0700

    update

--- js/validasi_masuk.js
+++ js/validasi_masuk.js
@@ -1,44 +1,27 @@
-var isMail = false;
-var isPass = false;
-var isEmail = false;
-function validasi(){
-  var mail = document.getElementById("myemail");
-  var pass = document.getElementById("mypass");
-  var cek = mail;
-        if (mail.value.length>0){
-      document.getElementById("email").style.display = "none";
-      isMail = true;
-      isEmail = false;
-      for (var i = 0; i < cek.value.length; i++){
-            var ch = cek.value.charAt(i);
-            if(ch == "@"){
-              isEmail = true;
-              break;
-              }
-           }
-        if (isEmail == false){
-          document.getElementById("email1").style.display = "block";
-        } else{
-          document.getElementById("email1").style.display = "none";
-        }
-    }else{
-      isMail = false;
-      document.getElementById("email").style.display = "block";
-      document.getElementById("email1").style.display = "none";
-
-    }
-    if (pass.value.length>0){
-        document.getElementById("pass").style.display = "none";
-        isPass = true;
-      }else{
-        document.getElementById("pass").style.display = "block";
-        isPass = false;
+$(document).ready(function() {
+	var form = $('button');
+  kosong = false;
+  
+	form.on('click', function (event) {
+		event.preventDefault();
+		var inputan = $(".konten").find("input");
+		console.log(inputan);
+		inputan.each(function(index, el) {
+			if ($(el).val() == "") {
+        console.log('inpus');
+        
+        kosong = true;
+			}
+			else{
+				console.log('asku');
       }
-
-}
-function lanjut(){
-  //lanjut ke halaman selanjutnya
-  if(isMail == true && isPass == true && isEmail==true){
-    window.location.href = "index-masuk.html";
-  }
-}
+      if(kosong){
+        console.log('sasa')
+        $('#email').css('display','block');
+      }
+      else{
+        $('#email').css('display','none')
+      }
+		});
+	});
+});
