ea4a433300ee168ea0b46adc80f42177bdcd8638
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-11-19 15:57:59 +0700

    Update lebihBanyak.js

--- js/lebihBanyak.js
+++ js/lebihBanyak.js
@@ -1,5 +1,15 @@
+var tampilBanyak = false;
 $(document).ready(function() {
   $(".btn-banyak").click(function(){
     $(".hidden-content").slideToggle();
+    if(tampilBanyak){
+      $(".btn-banyak > a").text("Tampilkan lebih banyak");
+      tampilBanyak = false;
+      $(".btn-banyak > i").removeClass("fa-rotate-180");
+    }else{
+      $(".btn-banyak > a").text("Tampilkan lebih sedikit");
+      tampilBanyak = true;
+      $(".btn-banyak > i").addClass("fa-rotate-180");
+    }
   });
 });
