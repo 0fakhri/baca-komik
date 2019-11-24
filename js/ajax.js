090a263e6b368df18e768aaf97e3ec991827af55
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-11-24 14:36:54 +0700

    ajax dengan getjson

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,14 @@
-$('#dark-mode').on('click', function(e) {
-  $.ajax({
-    url: 'http://jsonplaceholder.typicode.com/users',
-    method : "GET",
-  }).done(function(data){
-    console.log(data);
-  })
-});
+// $('#dark-mode').on('click', function(e) {
+//   $.ajax({
+//     url: 'http://jsonplaceholder.typicode.com/users',
+//     method : "GET",
+//   }).done(function(data){
+//     console.log(data);
+//   })
+// });
+
+$("#dark-mode").click(function(event){
+      $.getJSON('demo.json', function(data) {
+          console.log(data);
+      });
+  });
