1065084f880324ffdec41fb7338aeac1120ee5fa
h4rfu1 <mohfahrulhafidh@gmail.com>
2019-11-24 14:51:42 +0700

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,11 +1,11 @@
-// $('#dark-mode').on('click', function(e) {
-//   $.ajax({
-//     url: 'http://jsonplaceholder.typicode.com/users',
-//     method : "GET",
-//   }).done(function(data){
-//     console.log(data);
-//   })
-// });
+$('#dark-mode').on('click', function(e) {
+  $.ajax({
+    url: 'demo.json',
+    filetype : "json",
+  }).done(function(data){
+    console.log(data);
+  })
+});
 
 $("#dark-mode").click(function(event){
       $.getJSON('demo.json', function(data) {
