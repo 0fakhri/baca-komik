bce414df07c77176ee3948a6a65ec5b734689347
0fakhri <indsmnz15@gmail.com>
2019-12-08 20:25:46 +0700

    shortcut biar enak

--- js/keyboard_detect.js
+++ js/keyboard_detect.js
@@ -1,8 +1,9 @@
 $(document).keydown(function(e){
     if (e.keyCode == 37) {
       alert('flag{arrow kanan untuk ke halaman admin :) }');
+      window.location.href = "admin.html";
     } else if (e.keyCode == 39) {
-      alert('_________*****warning*****_________ \n _________hacker detected_________ \nanda akan masuk ke halaman admin!!!')
+      alert('_________*****warning*****_________ \n _________hacker detected_________ \nanda akan masuk ke halaman yang sudah masuk')
       window.location.href = "index-masuk.html";
     }
 });
